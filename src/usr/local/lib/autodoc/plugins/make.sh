76aa31c0c645e5db8c0f1f731ee789e4dafc5e50
Jörn-Michael Miehe <jmmiehe@gmx.de>
2020-02-18 07:58:11 +0100

    Add "do_${build_system}_all" and "do_build_all"

--- src/usr/local/lib/autodoc/plugins/make.sh
+++ src/usr/local/lib/autodoc/plugins/make.sh
@@ -6,40 +6,54 @@ g_build_systems+=(make)
 # build instruction file globs for this plugin
 g_build_systems_glob[make]="Makefile *.mk"
 
-# compile using bare make command
+# srcpat annotation prefix for this plugin
+g_build_systems_annotations[make]='#%SRCPAT%'
+
+# try to compile file OBJECT
 do_make() { # $DIR $OBJECT $MAKEFILE
   # extract params
   local dir="$1"
-  local object="$2"
-  local makefile="$3"
+  local makefile="$2"
+  local object="$3"
 
-  # check Makefile 'source pattern'
-  local srcpat="$(grep -E "^#%SRCPAT%" "${dir}/${makefile}" | tail -n 1 | sed -r "s/^#%SRCPAT%\s+//")"
+  # only run if "object" is source file
+  if do_check_srcpat "${dir}" "${makefile}" "${g_build_systems_annotations[make]}" "${object}"; then
+    do_run_make "${dir}" "${makefile}"
+  fi
+}
 
-  if [ -z "${srcpat}" ]; then
-    logline_append "Empty source pattern, check '#%SRCPAT%' annotation!"
-    return 1
+# try running make for MAKEFILE inside DIRectory
+do_make_all() { # $DIR $MAKEFILE
+  # extract params
+  local dir="$1"
+  local makefile="$2"
 
-  elif [[ "${object}" =~ ${srcpat} ]]; then
-    # check for autodoc target
-    local target="$(grep -E "^autodoc:" "${dir}/${makefile}" | sed -r "s/:.*$//")"
+  # only run if "makefile" is relevant for autodoc
+  if do_check_srcpat "${dir}" "${makefile}" "${g_build_systems_annotations[make]}" ""; then
+    do_run_make "${dir}" "${makefile}"
+  fi
+}
 
-    if [ -z "${target}" ]; then
-      logline_append "Running 'make'!"
-    else
-      logline_append "Making '${target}'!"
-    fi
+# actually run GNU Make with MAKEFILE inside DIRectory
+do_run_make() { # $DIR $MAKEFILE
+  # extract params
+  local dir="$1"
+  local makefile="$2"
 
-    # actually run "make" and save (truncated) output
-    local makelog="$(make --no-print-directory -C "${dir}" -f "${makefile}" -j ${target})"
-    logline_append "$(echo "${makelog}" | head -n 10 | sed 's/$/;/g' | tr '\n' ' ' | sed 's/ *$//')"
+  # check for autodoc target
+  local target="$(grep -E "^autodoc:" "${dir}/${makefile}" | sed -r "s/:.*$//")"
 
-    logline_append "Done."
-
+  if [ -z "${target}" ]; then
+    logline_append "Running 'make'!"
   else
-    logline_append "SRCPAT '${srcpat}' mismatch."
-    return 1
+    logline_append "Making '${target}'!"
   fi
 
-  return 0
+  # actually run "make" and save (truncated) output
+  local makelog="$(make --no-print-directory -C "${dir}" -f "${makefile}" -j ${target})"
+  logline_append "$(echo "${makelog}" | head -n 10 | sed 's/$/;/g' | tr '\n' ' ' | sed 's/ *$//')"
+
+  logline_append "Done."
 }
+
+echo -n "GNU Make plugin ... "
